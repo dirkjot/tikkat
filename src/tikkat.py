7718fb8e2e96cd82924c0e69e24d02327d6cec50
Dirk Janssen <djanssen@pivotal.io>
2019-06-12 16:42:02 -0700

    removed node code
    Signed-off-by: Tom Viehman <tviehman@pivotal.io>

--- src/tikkat.py
+++ src/tikkat.py
@@ -14,19 +14,38 @@ verification_token=os.environ.get('VERIFICATION_TOKEN')
 app = Flask(__name__)
 
 
+@app.route('/rj75ud/hello', methods=['GET'])
+def hello():
+    "Simple slash command"
+    return "Tikkat is up"
+
 @app.route('/rj75ud/slash', methods=['POST'])
 def slash():
     "Simple slash command"
-    if request.form['token'] == verification_token:
-        payload = {'text': 'Meow - Tikkat does not say much yet.'}
-        return jsonify(payload)
+    if request.form.get('token'):
+        if request.form['token'] == verification_token:
+            payload = {'text': 'Meow - Tikkat does not say much yet.'}
+            print(jsonify(payload))
+            return jsonify(payload)
+        print("Not token correct")
+        return "Not token correct"
+    else:
+        print("Not request correct")
+        return "Not request correct"
+
+
+@app.route('/rj75ud/oauth', methods=['POST'])
+def oauth():
+    "stub"
+    print("Oauth called")
+    return jsonify({})
 
 
 def setup_scheduler():
     "The AP Scheduler is used to run our airtable parse regularly"
 
     executors = {
-        'default': ThreadPoolExecutor(1) } 
+        'default': ThreadPoolExecutor(1) }
     job_defaults = {
         'coalesce': True,
         'max_instances': 3  }
@@ -39,4 +58,4 @@ def setup_scheduler():
 if __name__ == "__main__":
     setup_scheduler()
     app.run()
-    
+
