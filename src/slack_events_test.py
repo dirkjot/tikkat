c6ab2d278505ad065de394a61c22af5c8b7d68bc
Dirk Janssen <djanssen@pivotal.io>
2019-06-20 15:55:35 -0700

    Remove dead code

--- src/slack_events_test.py
+++ src/slack_events_test.py
@@ -12,27 +12,6 @@ def test_hello():
         assert b'Slack events up' in actual.data
 
 
-def XXtest_mention():
-    ""
-    event = {
-        'channel': 'CKHQ6FAMS',
-        'client_msg_id': 'acc0596d-b12f-4601-8c70-7f4e02e1c7d9',
-        'event_ts': '1560438906.001300',
-        'parent_user_id': 'U0AB10953',
-        'text': '<@UKHHR1BK2> thread',
-        'thread_ts': '1560438884.001000',
-        'ts': '1560438906.001300',
-        'type': 'app_mention',
-        'user': 'U0AB10953'}
-    event_data = {
-        'event': event,
-        'token': subject.verification_token}
-
-    with subject.app.test_client() as c:
-        actual = c.post('/slack/events', json=event_data)
-        assert b'Slack events up' in actual.data
-
-
 def test_close_ticket_in_thread():
     "this is a pretty horrible test but hard to change that in this architecture"
     event_data = {'event': {'channel': 'CKHQ6FAMS',
