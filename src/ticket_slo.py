cbc4ef710e250b7f87bd1c691c23f1c47cdb2f5a
Bozhidar Lenchov <blenchov@pivotal.io>
2019-06-28 10:56:33 -0400

    Added Airtable env defaults, fixes to slo endpoint

--- src/ticket_slo.py
+++ src/ticket_slo.py
@@ -16,6 +16,7 @@ from datetime import datetime
 from requests.exceptions import HTTPError
 from datetime import datetime, timedelta
 import pandas as pd
+import pytz
 from flask import Blueprint, request, abort, make_response, render_template
 from jinja2 import TemplateNotFound
 
@@ -33,7 +34,7 @@ slo = Blueprint('slo', __name__, template_folder='templates')
 def slo_main():
     "Main and only page for SLO"
     ontime, groupmeans, groupsizes = runstats(getdonedusted())
-    response = make_response(render_template('slo_page.html', 
+    response = make_response(render_template('slo_page.html',
         ontime=ontime, groupmeans=groupmeans, groupsizes=groupsizes))
     response.headers.set('Pragma','No-Cache')
     return response
@@ -41,8 +42,8 @@ def slo_main():
 
 def getdonedusted():
     "Retrieve donedusted table from airtable, in DataFrame format"
-    rawres = tickets_table.get_all(maxRecords=999, view="DoneDusted", 
-        fields=['DaysTaken','Urgency','SoftDeadline','Deadline', 'Completion'])                          
+    rawres = tickets_table.get_all(maxRecords=999, view="DoneDusted",
+        fields=['DaysTaken','Urgency','SoftDeadline','Deadline', 'Completion'])
     # Example record, note Deadline not returned as nil
     # {'id': 'recJdo3yte2NmnHAW',
     #  'fields': {'Urgency': 'Medium',
@@ -50,22 +51,22 @@ def getdonedusted():
     #   'DaysTaken': 0},
     #  'createdTime': '2019-06-04T06:42:20.000Z'}
 
-    da = pd.DataFrame([res['fields'] for res in rawres])    
+    da = pd.DataFrame([res['fields'] for res in rawres])
     return da
 
 def runstats(da):
     "Given donedusted data, compute stats"
     # replace empty values for Deadline with SoftDeadline
-    da.Deadline.fillna(da.SoftDeadline, inplace=True)  
+    da.Deadline.fillna(da.SoftDeadline, inplace=True)
     # compute SLO 1, assuming completion happened at start of day,
     # so completion *at* deadline date is passing the SLO
-    da['ontime'] = pd.to_datetime(da.Completion, utc=True) < pd.to_datetime(da.Deadline, utc=True) 
+    da['ontime'] = pd.to_datetime(da.Completion, utc=True) < pd.to_datetime(da.Deadline, utc=True)
     ontime = da.ontime.mean()
 
     # note that groupmeans need a double index (groupmeans['ontime']['High'])
     # but groupsizes does not, groupsizes['Low']
-    groupmeans = da.groupby('Urgency').mean()                                                                                                                                                        
-    groupsizes = da.groupby('Urgency').size() 
+    groupmeans = da.groupby('Urgency').mean()
+    groupsizes = da.groupby('Urgency').size()
 
     # visual formatting of percentages and averages
     ontime = round(ontime*100, 0)
@@ -73,4 +74,3 @@ def runstats(da):
     groupmeans['DaysTaken'] = round(groupmeans.DaysTaken, 1)
 
     return ontime, groupmeans, groupsizes
-
